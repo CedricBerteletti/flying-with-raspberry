b5f25df46439e15fe93fe83bb2fcae6642702f78
Cédric Berteletti <47115606+CedricBerteletti@users.noreply.github.com>
2026-05-03 17:16:17 +0200

    Fix last picture number

--- flight-raspberry/services/image_processing.py
+++ flight-raspberry/services/image_processing.py
@@ -14,25 +14,22 @@ class ImageProcessing(metaclass=SingletonMeta):
         self.PICTURES_NAME = settings.get("pictures.name")
         self.PICTURES_FORMAT = settings.get("pictures.format")
 
-        # Initialize the picture counter based on existing pictures
-        self.get_last_picture_id()
+        self.num_picture = 0
 
 
     def take_picture(self):
         time_str = datetime.date.today().strftime(self.TIME_FORMAT)
-        self.num_picture = self.num_picture + 1
+        self.num_picture = self.get_last_picture_id() + 1
 
-        file_name = f"{self.FOLDER}{time_str}-{self.PICTURES_NAME}-{str(self.num_picture)}.{self.PICTURES_FORMAT}"
+        file_name = f"{self.FOLDER}{time_str}-{self.PICTURES_NAME}-{str(self.get_last_picture_id())}.{self.PICTURES_FORMAT}"
         if not os.path.exists(self.FOLDER):
             os.makedirs(self.FOLDER)
         os.system(f"rpicam-still -o {file_name} -t 1ms")
         
-        return self.num_picture, file_name
+        return self.get_last_picture_id(), file_name
     
     def get_last_picture_id(self):
-        if self.num_picture is None:
-            self.num_picture = 0
-
+        # Initialize the picture counter based on existing pictures
         if self.num_picture == 0 and os.path.exists(self.FOLDER):
             # Scan existing pictures to set the num_picture counter
             for file in glob.glob(f"{self.FOLDER}*-{self.PICTURES_NAME}-*.{self.PICTURES_FORMAT}"):
